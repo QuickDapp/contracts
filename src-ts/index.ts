f9e88cdf8eaade989eb2410d59e397e5d6999704
Ramesh Nair <ram@hiddentao.com>
2023-11-24 16:31:34 +0000

    chore: only export abi

--- src-ts/index.ts
+++ src-ts/index.ts
@@ -1,7 +1,2 @@
 import { Abi } from 'abitype'
-import type { TargetDeploymentRecords } from "gemforge/build/shared/chain"
-
-export const deployedAddresses =
-  require("../gemforge.deployments.json") as TargetDeploymentRecords
-
 export const abi = require("../src/generated/abi.json") as Abi
