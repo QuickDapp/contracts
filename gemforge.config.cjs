56def5eb012940cecbbaa6c8ea4fd25091a192f9
Ramesh Nair <ram@hiddentao.com>
2024-02-14 10:31:17 +0530

    feat: upgrade Gemforge and use CREATE3 salt to fix address

--- gemforge.config.cjs
+++ gemforge.config.cjs
@@ -3,54 +3,52 @@ module.exports = {
   version: 2,
   solc: {
     // SPDX License - to be inserted in all generated .sol files
-    license: 'MIT',
+    license: "MIT",
     // Solidity compiler version - to be inserted in all generated .sol files
-    version: '0.8.21',
+    version: "0.8.21",
   },
   // commands to execute
   commands: {
     // the build command
-    build: 'forge build --names --sizes',
+    build: "forge build --names --sizes",
   },
   paths: {
     // contract built artifacts folder
-    artifacts: 'out',
+    artifacts: "out",
     // source files
     src: {
       // file patterns to include in facet parsing
       facets: [
         // include all .sol files in the facets directory ending "Facet"
-        'src/facets/*Facet.sol'
+        "src/facets/*Facet.sol",
       ],
     },
     // folders for gemforge-generated files
     generated: {
       // output folder for generated .sol files
-      solidity: 'src/generated', 
+      solidity: "src/generated",
       // output folder for support scripts and files
-      support: '.gemforge',
+      support: ".gemforge",
       // deployments JSON file
-      deployments: 'gemforge.deployments.json',
+      deployments: "gemforge.deployments.json",
     },
     // library source code
     lib: {
       // diamond library
-      diamond: 'lib/diamond-2-hardhat',
-    }
+      diamond: "lib/diamond-2-hardhat",
+    },
   },
   // artifacts configuration
   artifacts: {
     // artifact format - "foundry" or "hardhat"
-    format: 'foundry'
+    format: "foundry",
   },
   // generator options
   generator: {
     // proxy interface options
     proxyInterface: {
       // imports to include in the generated IDiamondProxy interface
-      imports: [
-        "src/shared/Structs.sol",
-      ],
+      imports: ["src/shared/Structs.sol"],
     },
   },
   // diamond configuration
@@ -60,47 +58,43 @@ module.exports = {
     // The diamond initialization contract - to be called when first deploying the diamond.
     init: {
       // The diamond initialization contract name
-      contract: 'InitDiamond',
+      contract: "InitDiamond",
       // The diamond initialization function name
-      function: 'init',
-    },  
+      function: "init",
+    },
     // Names of core facet contracts - these will not be modified/removed once deployed and are also reserved names.
     // This default list is taken from the diamond-2-hardhat library.
     // NOTE: we recommend not removing any of these existing names unless you know what you are doing.
-    coreFacets: [
-      'OwnershipFacet',
-      'DiamondCutFacet',
-      'DiamondLoupeFacet',
-    ],
+    coreFacets: ["OwnershipFacet", "DiamondCutFacet", "DiamondLoupeFacet"],
   },
   // lifecycle hooks
   hooks: {
     // shell command to execute before build
-    preBuild: '',
+    preBuild: "",
     // shell command to execute after build
-    postBuild: '',
+    postBuild: "",
     // shell command to execute before deploy
-    preDeploy: '',
+    preDeploy: "",
     // shell command to execute after deploy
-    postDeploy: 'scripts/verify.js',
+    postDeploy: "scripts/verify.js",
   },
   // Wallets to use for deployment
   wallets: {
     // Wallet named "wallet1"
     wallet1: {
       // Wallet type - mnemonic
-      type: 'mnemonic',
+      type: "mnemonic",
       // Wallet config
       config: {
         // Mnemonic phrase
-        words: 'test test test test test test test test test test test junk',
+        words: "test test test test test test test test test test test junk",
         // 0-based index of the account to use
         index: 0,
-      }
+      },
     },
     wallet2: {
       // Wallet type - mnemonic
-      type: 'mnemonic',
+      type: "mnemonic",
       // Wallet config
       config: {
         // Mnemonic phrase
@@ -115,31 +109,37 @@ module.exports = {
     // Local network
     local: {
       // RPC endpoint URL
-      rpcUrl: 'http://localhost:8545',
+      rpcUrl: "http://localhost:8545",
     },
     // Sepolia network
     sepolia: {
       // RPC endpoint URL
       rpcUrl: () => process.env.SEPOLIA_RPC_URL,
-    }
+    },
   },
   // Targets to deploy
   targets: {
     local: {
       // Network to deploy to
-      network: 'local',
+      network: "local",
       // Wallet to use for deployment
-      wallet: 'wallet1',
+      wallet: "wallet1",
       // Initialization function arguments
       initArgs: [],
+      // CREATE3 salt
+      create3Salt:
+        "0xde2f926c7dd655eb264988dcee29e9237ce8662868da5c82bb0988c159e5be55",
     },
     sepolia: {
       // Network to deploy to
-      network: 'sepolia',
+      network: "sepolia",
       // Wallet to use for deployment
-      wallet: 'wallet2',
+      wallet: "wallet2",
       // Initialization function arguments
       initArgs: [],
-    }
-  }
-}
+      // CREATE3 salt
+      create3Salt:
+        "0xde2f926c7dd655eb264988dcee29e9237ce8662868da5c82bb0988c159e5be55",
+    },
+  },
+};
